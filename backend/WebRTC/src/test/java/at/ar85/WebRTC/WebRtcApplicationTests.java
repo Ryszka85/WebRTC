b30c4a20c43621128b7b5c177488dde551562667
Adrian Ryszka <adrian.ryszka@gmx.net>
2021-11-13 21:15:48 +0100

    Project setup

---
+++ backend/WebRTC/src/test/java/at/ar85/WebRTC/WebRtcApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ar85.WebRTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
